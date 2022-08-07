5891ee5a65c9f10db5688c59f1894d00cb86929c
NeverJar <109418390+NeverJar@users.noreply.github.com>
2022-08-07 19:12:53 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Typewriter Effect for Python!'
+LONG_DESCRIPTION = 'This package allows you to use the Typewriter Effect in Python.'
+
+# Setting up
+setup(
+    name="typewriter",
+    version=VERSION,
+    author="Hayoto",
+    author_email="<mail@hayoto.lol>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'type', 'print', 'typewriter'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
